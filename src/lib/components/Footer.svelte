e1f8036621ac7984a3274d8f46d38e9ac8a1a2bb
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2026-02-02 18:31:49 +0300

    c

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -180,7 +180,7 @@
 		</div>
 		<div class="mt-8 border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
 			<p class="mt-8 text-sm/6 text-gray-400 md:order-1 md:mt-0">
-				&copy; 2025 Компания "Новострой". Все права защищены.
+				&copy; {new Date().getFullYear()} Компания "Новострой". Все права защищены.
 			</p>
 		</div>
 	</div>
